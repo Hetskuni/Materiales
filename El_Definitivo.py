14596c948657843ae78321db60295d595cce78fa
Hetskuni <arangosanti3@gmail.com>
2024-08-28 20:12:40 -0500

    Update El_Definitivo.py

--- El_Definitivo.py
+++ El_Definitivo.py
@@ -2,8 +2,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Paso 0: Elegir que base de datos segun el numero de la probeta
+# Probeta 1: https://raw.githubusercontent.com/Hetskuni/Materiales/main/DatosEnsayoProbeta_1.csv
+# Probeta 2: https://raw.githubusercontent.com/Hetskuni/Materiales/main/DatosEnsayoProbeta_2.csv
+# Probeta 3: https://raw.githubusercontent.com/Hetskuni/Materiales/main/DatosEnsayoProbeta_3.csv
+# Probeta 4: https://raw.githubusercontent.com/Hetskuni/Materiales/main/DatosEnsayoProbeta_4.csv
+
 # Paso 1: Leer y procesar el archivo completo
-df = pd.read_csv('https://raw.githubusercontent.com/JuanAraque11/Repo-prueba-PPI/main/DatosEnsayo_1_csv.csv', sep=';')
+df = pd.read_csv('https://raw.githubusercontent.com/Hetskuni/Materiales/main/DatosEnsayoProbeta_1.csv', sep=';')
 df = df.drop(index=df.index[0])
 
 # Convertir a numérico y eliminar filas no numéricas
@@ -13,7 +19,6 @@ columnas = ['Deformación por compresión', 'Esfuerzo de compresión']
 df[columnas] = df[columnas].replace({',': '.'}, regex=True)
 df[columnas] = df[columnas].apply(pd.to_numeric)
 
-# Calcular la tenacidad
 # Calcular la tenacidad
 tenacidad = np.trapz(df['Esfuerzo de compresión'], df['Deformación por compresión'])
 print(f"Tenacidad calculada: {tenacidad:.10f} MPa")
